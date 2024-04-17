e82ca386e57d6cdb1bda5af068b8d9903a8dbeee
Maksym Shapovalov <maks0681912507@gmail.com>
2024-04-16 21:44:23 -0400

    https://leetcode.com/problems/fizz-buzz/

---
+++ 412. Fizz Buzz.ts
@@ -0,0 +1,10 @@
+function fizzBuzz(n: number): string[] {
+    const answer: string[] = new Array(n);
+    for (let i = 1; i <= n; i++) {
+        if (!(i % 3) && !(i % 5)) answer[i - 1] = "FizzBuzz";
+        else if (!(i % 3)) answer[i - 1] = "Fizz";
+        else if (!(i % 5)) answer[i - 1] = "Buzz";
+        else answer[i - 1] = i.toString();
+    }
+    return answer;
+};
